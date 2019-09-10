2f8dbed7d526e50a2142c07a39604c12041db690
homin <homin722@naver.com>
2019-09-10 16:51:45 +0900

    팀장 homin 작업시작!!

--- 04_FinalGitTest/src/main/java/com/kh/spring/FinalGitTest.java
+++ 04_FinalGitTest/src/main/java/com/kh/spring/FinalGitTest.java
@@ -4,5 +4,6 @@ public class FinalGitTest {
 	public void test() {
 		System.out.println("팀장이 프로젝트 시작");
 		System.out.println("팀원 auhe987가 작업을 시작했습니다~!!");
+		System.out.println("팀장 homin도 작업을 시작!!!");
 	}
 }
